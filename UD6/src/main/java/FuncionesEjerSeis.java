23950ac24b2bc112f61564a4171a9177cb2f7b6f
IN1DM3B_18 <IN1DM3B_18@H015112.NET>
2019-01-11 14:23:28 +0100

    todo guardado

--- UD6/src/main/java/FuncionesEjerSeis.java
+++ UD6/src/main/java/FuncionesEjerSeis.java
@@ -1,6 +1,12 @@
 
 public class FuncionesEjerSeis {
 
+	public float getLibras(float peso) {
+		float pesoLibras=0;
+		pesoLibras=(float) (peso*(1/0.453));	
+		return pesoLibras;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- UD6/src/main/java/Peso.java
+++ UD6/src/main/java/Peso.java
@@ -1,4 +1,14 @@
 
 public class Peso {
-
+	//Atributos
+	public float peso;
+	private String medida;
+	
+	public Peso (float peso,String medida){
+		this.peso=0;
+		this.medida="";
+	}
+	
+	
+	
 }
